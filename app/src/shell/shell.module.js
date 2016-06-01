1956408535b60e4efdbc9b722cf63d0bf3a06a0b
Stevey Z <sjzerneri@gmail.com>
2016-05-31 21:32:59 -0400

    adding shell

---
+++ app/src/shell/shell.module.js
@@ -0,0 +1,28 @@
+/**
+ * @ngdoc overview
+ * @name app.shell
+ * @description < description placeholder >
+ */
+
+(function(){
+
+  'use strict';
+
+  angular
+    .module('app.shell', [])
+    .config(configuration);
+
+  /* @ngInject */
+  function configuration($stateProvider){
+
+    //add your state mappings here
+    //$stateProvider
+    //  .state('Welcome', {
+    //    url:'/welcome',
+    //    templateUrl:'src/welcome/welcome.html',
+    //    controller: 'WelcomeCtrl as vm'
+    //  }
+    //);
+  }
+
+}());
