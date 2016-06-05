38c04f304e962f7e7e56379cd0b1b79deb2e22bb
Stevey Z <sjzerneri@gmail.com>
2016-06-05 15:56:44 -0400

    Initial Commit

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,115 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "app",
+      "shortName": "app",
+      "type": "overview",
+      "moduleName": "app",
+      "shortDescription": "Glue to where all the greatness begins",
+      "keywords": "api app glue greatness overview"
+    },
+    {
+      "section": "api",
+      "id": "app.catalog",
+      "shortName": "app.catalog",
+      "type": "overview",
+      "moduleName": "app.catalog",
+      "shortDescription": "&lt; description placeholder &gt;",
+      "keywords": "api app catalog description overview placeholder"
+    },
+    {
+      "section": "api",
+      "id": "app.checkout",
+      "shortName": "app.checkout",
+      "type": "overview",
+      "moduleName": "app.checkout",
+      "shortDescription": "&lt; description placeholder &gt;",
+      "keywords": "api app checkout description overview placeholder"
+    },
+    {
+      "section": "api",
+      "id": "app.common",
+      "shortName": "app.common",
+      "type": "overview",
+      "moduleName": "app.common",
+      "shortDescription": "Host of all the cross cutting source",
+      "keywords": "api app common cross cutting host overview source"
+    },
+    {
+      "section": "api",
+      "id": "app.core",
+      "shortName": "app.core",
+      "type": "overview",
+      "moduleName": "app.core",
+      "shortDescription": "Core is where the Magma is",
+      "keywords": "api app core magma overview"
+    },
+    {
+      "section": "api",
+      "id": "app.core",
+      "shortName": "app.core",
+      "type": "overview",
+      "moduleName": "app.core",
+      "shortDescription": "Configuration block for restangular",
+      "keywords": "api app block configuration core overview restangular"
+    },
+    {
+      "section": "api",
+      "id": "app.core",
+      "shortName": "app.core",
+      "type": "overview",
+      "moduleName": "app.core",
+      "shortDescription": "Configuration block for routing",
+      "keywords": "api app block configuration core overview routing"
+    },
+    {
+      "section": "api",
+      "id": "app.core.constant:httpStatus",
+      "shortName": "httpStatus",
+      "type": "object",
+      "moduleName": "app.core",
+      "shortDescription": "Commonly consumed HTTP Request Codes exposed as an injectable object",
+      "keywords": "accepted angular api app bad_gateway bad_request codes commonly constant consumed controller core created data exposed forbidden function http httpstatus injectable internal_server_error method_not_allowed module no_content not_found object property request res service_unavailable somemodule someservice status unauthorized users var vm"
+    },
+    {
+      "section": "api",
+      "id": "app.shell",
+      "shortName": "app.shell",
+      "type": "overview",
+      "moduleName": "app.shell",
+      "shortDescription": "&lt; description placeholder &gt;",
+      "keywords": "api app description overview placeholder"
+    },
+    {
+      "section": "api",
+      "id": "app.shell.controller:Home",
+      "shortName": "Home",
+      "type": "controller",
+      "moduleName": "app.shell",
+      "shortDescription": "&lt; description placeholder &gt;",
+      "keywords": "api app controller description method num number placeholder rules testfunction"
+    },
+    {
+      "section": "api",
+      "id": "app.wizard",
+      "shortName": "app.wizard",
+      "type": "overview",
+      "moduleName": "app.wizard",
+      "shortDescription": "&lt; description placeholder &gt;",
+      "keywords": "api app description overview placeholder wizard"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
